de10f083534f807cadc581682ca1c80bfa9fb87d
devbence <936368+devbence@users.noreply.github.com>
2023-12-02 19:42:13 +0100

    sauce for 1.0, .net 4.7.2, c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace BLH4S
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.localips = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // localips
+            // 
+            this.localips.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.localips.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.localips.FormattingEnabled = true;
+            this.localips.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.localips.Location = new System.Drawing.Point(70, 100);
+            this.localips.Margin = new System.Windows.Forms.Padding(5);
+            this.localips.Name = "localips";
+            this.localips.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.localips.Size = new System.Drawing.Size(160, 93);
+            this.localips.Sorted = true;
+            this.localips.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "copy selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "select your ip from the list, then click on copy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "&test sit connection";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(56, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 69);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(15, 246);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 92);
+            this.panel1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Location = new System.Drawing.Point(70, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 93);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "&CLICK TO SHOW IPs";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "http://";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(15, 199);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(327, 41);
+            this.panel2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(202, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "6969";
+            this.textBox3.DoubleClick += new System.EventHandler(this.textBox3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.groupBox1.Location = new System.Drawing.Point(13, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 92);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "&test results";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 20);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(316, 64);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "// set the hostname / ip above, and click test to see the results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 342);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.localips);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "blh.s - bullet\'s little helper for SIT";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ListBox localips;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BLH4S
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CollectAndFillIPv4Addresses();
+        }
+
+        /// <summary>
+        /// Test button click actions
+        /// </summary>
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var sitHost = textBox1.Text.Trim();
+            Console.WriteLine("PortKnocking " + sitHost);
+            richTextBox1.Clear();
+            richTextBox1.AppendText("[ Testing connection... please wait.. ]");
+            disableFloodAsync();
+            var port6969Open = await isPortOpenAsync(sitHost, 6969);
+            var port6970Open = await isPortOpenAsync(sitHost, 6970);
+
+            // Clear previous text
+            richTextBox1.Clear();
+            // Append colored text
+            AppendColoredText("Port 6969 (AKI): ", GetColorForStatus(port6969Open));
+            AppendColoredText(port6969Open, GetColorForStatus(port6969Open));
+            richTextBox1.AppendText("\r\n"); // Newline
+
+            AppendColoredText("Port 6970 (SIT): ", GetColorForStatus(port6970Open));
+            AppendColoredText(port6970Open, GetColorForStatus(port6970Open));
+        }
+
+        private Color GetColorForStatus(string status)
+        {
+            return status == "AVAILABLE" ? Color.Green : Color.Red;
+        }
+
+
+        private void AppendColoredText(string text, Color color)
+        {
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.SelectionLength = 0;
+
+            richTextBox1.SelectionColor = color;
+            richTextBox1.AppendText(text);
+            richTextBox1.SelectionColor = richTextBox1.ForeColor;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button3.Hide();
+            button1.Visible = true;
+        }
+
+        /// <summary>
+        /// disables the Test button for 9s
+        /// </summary>
+        /// <returns></returns>
+        private async Task disableFloodAsync()
+        {
+            button2.Enabled = false;
+            await Task.Delay(9000);
+            button2.Enabled = true;
+        }
+        /// <summary>
+        /// the port knocker itself
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        private async Task<string> isPortOpenAsync(string host, int port)
+        {
+            try
+            {
+                using (TcpClient tester = new TcpClient())
+                {
+                    await tester.ConnectAsync(host, port);
+                    tester.NoDelay = true;
+                    tester.SendTimeout = 2500;
+                    tester.ReceiveTimeout = 2500;
+                    return "AVAILABLE";
+                }
+            }
+            catch (SocketException)
+            {
+                return "NOT AVAILABLE";
+            }
+            catch (Exception)
+            {
+                return "NOT AVAILABLE";
+            }
+        }
+
+        /// <summary>
+        /// Collect the IPs from System, and puts it in the localips textbox
+        /// </summary>
+        private void CollectAndFillIPv4Addresses()
+        {
+            foreach (IPAddress ip in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) // Check if it's IPv4
+                {
+                    localips.Items.Add(ip.ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// sends the selected ip to clipboard
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (localips.SelectedItem != null)
+            {
+                Clipboard.SetText(localips.SelectedItem.ToString());
+            }
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.ReadOnly = false;
+            textBox3.Enabled = true;
+        }
+    }
+}
